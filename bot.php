0cbf2a0940e7d6748b854f7b3befdf00d0315646
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 17:55:00 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,14 +19,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'halo'
+				'text' => '$text'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => ['haloo'],
+				'messages' => ['$message'],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
