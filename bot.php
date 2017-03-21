9ba0e1f754a459295cfe31dea59cc1e035ad4ebf
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 17:41:21 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,27 +16,23 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-		;
-if($message['type']=='text')
-{
-	if($pesan_datang=='1')
-	{
-		
-		
-		$balas = array(
-							'replyToken' => $replyToken,														
-							'messages' => array(
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => array(
 								array(
 										'type' => 'text',					
 										'text' => 'Halo '.$profil->displayName.', Anda memilih menu 1,'
 									)
 							)
 						);
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
