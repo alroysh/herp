8571faec38490dbe9476db7744590d2a7a346db4
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 18:13:54 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,13 +2,13 @@
 
 curl -X POST \
 -H 'Content-Type:application/json' \
--H 'Authorization: Bearer {foHivR9RW1cwM7LwHhSBOPTAjGa8o8kbmomtLhC906UPPWoB1gIsMhCXh7oE9bGA4HcnU1iGygo06OflcHmU827yGqF3qGQtwPReKwcx+QTOKHKqRFcCDFysPvqeHESKUm4Ey4gPabfHkJeT5FzOdQdB04t89/1O/w1cDnyilFU=}' \
+-H 'Authorization: Bearer foHivR9RW1cwM7LwHhSBOPTAjGa8o8kbmomtLhC906UPPWoB1gIsMhCXh7oE9bGA4HcnU1iGygo06OflcHmU827yGqF3qGQtwPReKwcx+QTOKHKqRFcCDFysPvqeHESKUm4Ey4gPabfHkJeT5FzOdQdB04t89/1O/w1cDnyilFU=' \
 -d '{
     "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
     "messages":[
         {
             "type":"text",
-            "text":"Hello, user"
+            "text":"Hello"
         },
         {
             "type":"text",
