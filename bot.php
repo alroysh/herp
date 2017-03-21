dc4c69f8fc9253f5ce5a8c100e8d0bc20f415d9c
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 17:42:20 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,13 +26,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => 'Halo '.$profil->displayName.', Anda memilih menu 1,'
-									)
-							)
-						);
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
