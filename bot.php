b533eb519404462253ee6e4a8c08b7838218599c
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 18:31:54 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,8 +4,8 @@ copyright @ medantechno.com
 2017
 */
 require_once('./line_class.php');
-$channelAccessToken = ''; //sesuaikan 
-$channelSecret = '';//sesuaikan
+$channelAccessToken = 'foHivR9RW1cwM7LwHhSBOPTAjGa8o8kbmomtLhC906UPPWoB1gIsMhCXh7oE9bGA4HcnU1iGygo06OflcHmU827yGqF3qGQtwPReKwcx+QTOKHKqRFcCDFysPvqeHESKUm4Ey4gPabfHkJeT5FzOdQdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = 'c1ab49d4f21251d9632f634a25605d24';//sesuaikan
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 //var_dump($client->parseEvents());
 //$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
