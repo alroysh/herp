39de77f600a70b47a1f4077672b2d5c675212805
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 19:39:22 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,6 +3,7 @@
 	copyright @ medantechno.com
 	2017
 	*/
+	date_default_timezone_set('Asia/Singapore');
 	require_once('./line_class.php');
 	$channelAccessToken = 'foHivR9RW1cwM7LwHhSBOPTAjGa8o8kbmomtLhC906UPPWoB1gIsMhCXh7oE9bGA4HcnU1iGygo06OflcHmU827yGqF3qGQtwPReKwcx+QTOKHKqRFcCDFysPvqeHESKUm4Ey4gPabfHkJeT5FzOdQdB04t89/1O/w1cDnyilFU='; //sesuaikan 
 	$channelSecret = 'c1ab49d4f21251d9632f634a25605d24';//sesuaikan
@@ -33,7 +34,6 @@
 	$profil = $client->profil($userId);
 	$pesan_datang = $message['text'];
 	$wita= date_default_timezone_set('Asia/Singapore');
-	$jam = date("H.i.s ");
 	//pesan bergambar
 	if($message['type']=='text')
 	{
@@ -109,7 +109,7 @@
 								'messages' => array(
 									array(
 											'type' => 'text',					
-											'text' => 'Jam Server Saya : '$jam
+											'text' => 'Jam Server Saya : '. date('H:i:s')
 										)
 								)
 							);
