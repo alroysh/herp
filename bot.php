87dda64ea9c5d528f795224ba2d8d090e20ceaa2
alroysh <cicak.jatuh02@gmail.com>
2017-03-21 20:54:18 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -47,7 +47,7 @@
 								'messages' => array(
 									array(
 											'type' => 'text',					
-											'text' => 'Halo '.$profil->displayName.''
+											'text' => 'Halo 􀂲'.$profil->displayName.''
 										)
 								)
 							);
