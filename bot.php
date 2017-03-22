67430900102b1bf7c252f9dd75572eff968063ef
alroysh <cicak.jatuh02@gmail.com>
2017-03-22 12:55:56 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -62,24 +62,8 @@
 								'replyToken' => $replyToken,														
 								'messages' => array(
 									array(
-											 "type": "template",
-  "altText": "this is a confirm template",
-  "template": {
-      "type": "confirm",
-      "text": "Are you sure?",
-      "actions": [
-          {
-            "type": "message",
-            "label": "Yes",
-            "text": "yes"
-          },
-          {
-            "type": "message",
-            "label": "No",
-            "text": "no"
-          }
-      ]
-  }
+										'type' => 'text',					
+										'text' => 'Halo'.$profil->mid.''	
   }
 										)
 								)
