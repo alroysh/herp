6dfb5e4bc0f44b4c0f21e463dba5380833da8847
alroysh <cicak.jatuh02@gmail.com>
2017-03-22 13:55:27 +0800

    Create coba.php

---
+++ coba.php
@@ -0,0 +1,63 @@
+/* 輸入申請的Line Developers 資料  */
+	$channel_id = "{1495903800}";
+	$channel_secret = "{c1ab49d4f21251d9632f634a25605d24}";
+	$channel_access_token = "{foHivR9RW1cwM7LwHhSBOPTAjGa8o8kbmomtLhC906UPPWoB1gIsMhCXh7oE9bGA4HcnU1iGygo06OflcHmU827yGqF3qGQtwPReKwcx+QTOKHKqRFcCDFysPvqeHESKUm4Ey4gPabfHkJeT5FzOdQdB04t89/1O/w1cDnyilFU=}";
+
+
+//  當有人發送訊息給bot時 我們會收到的json
+// 	{
+// 	  "events": 
+// 	  [
+// 		  {
+// 			"replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+// 			"type": "message",
+// 			"timestamp": 1462629479859,
+// 			"source": {
+// 				 "type": "user",
+// 				 "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+// 			 },
+// 			 "message": {
+// 				 "id": "325708",
+// 				 "type": "text",
+// 				 "text": "Hello, world"
+// 			  }
+// 		  }
+// 	  ]
+// 	}
+	 
+	 
+	// 將收到的資料整理至變數
+	$receive = json_decode(file_get_contents("php://input"));
+	
+	// 讀取收到的訊息內容
+	$text = $receive->events[0]->message->text;
+	
+	// 讀取訊息來源的類型 	[user, group, room]
+	$type = $receive->events[0]->source->type;
+	
+	// 由於新版的Messaging Api可以讓Bot帳號加入多人聊天和群組當中
+	// 所以在這裡先判斷訊息的來源
+	if ($type == "room")
+	{
+		// 多人聊天 讀取房間id
+		$from = $receive->events[0]->source->roomId;
+	} 
+	else if ($type == "group")
+	{
+		// 群組 讀取群組id
+		$from = $receive->events[0]->source->groupId;
+	}
+	else
+	{
+		// 一對一聊天 讀取使用者id
+		$from = $receive->events[0]->source->userId;
+	}
+	
+	// 讀取訊息的型態 [Text, Image, Video, Audio, Location, Sticker]
+	$content_type = $receive->events[0]->message->type;
+	
+	// 準備Post回Line伺服器的資料 
+	$header = ["Content-Type: application/json", "Authorization: Bearer {" . $channel_access_token . "}"];
+	
+	// 回覆訊息
+	reply($content_type, $text);
