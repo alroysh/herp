8817b2ecdc70da5d4d6864d31eee5f44303284a4
alroysh <cicak.jatuh02@gmail.com>
2017-03-22 11:29:39 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -54,6 +54,45 @@
 					
 		}
 		else
+			if($pesan_datang=='/coba')
+		{
+			
+			
+			$balas = array(
+								'replyToken' => $replyToken,														
+								'messages' => array(
+									array(
+											 "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+										)
+								)
+							);
+					
+		}
+		else
 				if($pesan_datang=='stickernya mana?')
 		{
 			
